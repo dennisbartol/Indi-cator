e105872d964f1b4de6158ea27ec575248a8af6b1
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-01-30 12:58:56 +0100

    Update scrollcalc.js

--- scrollcalc.js
+++ scrollcalc.js
@@ -2,6 +2,8 @@ const progressBar = document.querySelector(".progress-bar");
 const height = document.documentElement.scrollHeight - document.documentElement.clientHeight; 
 
 windows.addEventListener("scroll", () => {
+  const scrollTop = document.documentElement.scrollTop; 
+  const scrolled = (scrollTop / height) * 100; 
 
 
 });
